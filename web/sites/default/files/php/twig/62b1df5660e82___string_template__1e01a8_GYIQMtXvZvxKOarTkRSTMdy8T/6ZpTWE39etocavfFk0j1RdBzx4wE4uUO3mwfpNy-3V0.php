36125d3f1395862103efccc71394ed2fb654451d
NataliiaNychyporchuk <natikua1996@gmail.com>
2022-06-21 18:24:20 +0300

    first commit

---
+++ web/sites/default/files/php/twig/62b1df5660e82___string_template__1e01a8_GYIQMtXvZvxKOarTkRSTMdy8T/6ZpTWE39etocavfFk0j1RdBzx4wE4uUO3mwfpNy-3V0.php
@@ -0,0 +1,85 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__1e01a86ba689fc69ba617cec56fd3e829d4de9810b2971f2147692039d269c5d */
+class __TwigTemplate_fafd64474e5fab839b9a5bc1b8eef8c78d5ffbead9b14561f2f1a0a6d5c1a979 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "Lorem Ipsum is simply dummy text of the print-<br>
+ing and typesetting industry. ";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__1e01a86ba689fc69ba617cec56fd3e829d4de9810b2971f2147692039d269c5d";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__1e01a86ba689fc69ba617cec56fd3e829d4de9810b2971f2147692039d269c5d", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
